407485e27c782723395bf1b3e1e7578383f6cc40
Logamm <39797648+Logamm@users.noreply.github.com>
2018-05-31 14:13:28 +0200

    primer commit
    Primer commit...

---
+++ EjemploMaven/src/main/java/com/sunion/cursoJava/App.java
@@ -0,0 +1,14 @@
+package com.sunion.cursoJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	// cambio primer commit
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World 1!" );
+    }
+}
